59c7a281ddd1fdff5a3e1c8c78b61585df7a1b43
olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2023-11-26 22:12:32 +0100

    With regex

---
+++ target/debug/deps/grep_lite_regex-d5db9c9d65f9d318.d
@@ -0,0 +1,5 @@
+/Users/sam/Documents/axe-sharpening/rust/grep-lite-regex/target/debug/deps/grep_lite_regex-d5db9c9d65f9d318: src/main.rs
+
+/Users/sam/Documents/axe-sharpening/rust/grep-lite-regex/target/debug/deps/grep_lite_regex-d5db9c9d65f9d318.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/grep-lite-regex.d
@@ -0,0 +1,1 @@
+/Users/sam/Documents/axe-sharpening/rust/grep-lite-regex/target/debug/grep-lite-regex: /Users/sam/Documents/axe-sharpening/rust/grep-lite-regex/src/main.rs
